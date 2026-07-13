efc744a97425c80837c5b33af5acfc8388d6e898
a9r1311 <a9r1311@outlook.jp>
2026-07-13 11:16:26 +0900

    ユーザー認証のための作業
    ・UserData構造体定義
    ・CalculationProxyClass定義(未完)

--- Move/Assets/Script/C#/NetWork/Packet/PacketType.cs
+++ Move/Assets/Script/C#/NetWork/Packet/PacketType.cs
@@ -3,11 +3,12 @@ namespace Move.Packet
     //  パケット先頭1Byteに詰める識別用Enum
     public enum PacketType : byte
     {
-        Message        = 0,    //  Stringパケット
-        AssignPlayerId = 1,    //  プレイヤーID割り振りパケット
-        Position       = 2,    //  座標パケット
-        GoMatch        = 3,    //  マッチ通知用パケット
-        MatchSuccess   = 4,    //  マッチ成功パケット
-        SpawnPlayer    = 5,    //  プレイヤー生成パケット
+        Message = 0,         // Stringパケット
+        AssignPlayerId = 1,  // プレイヤーID割り振りパケット
+        Login = 2,           // ログイン要求パケット
+        GoMatch = 3,         // マッチ通知用パケット
+        MatchSuccess = 4,    // マッチ成功パケット
+        SpawnPlayer = 5,     // プレイヤー生成パケット
+        Position = 6,        // 座標パケット
     }
 }
--- Server/InGameServer/PacketType.cs
+++ Server/InGameServer/PacketType.cs
@@ -3,11 +3,12 @@
     //  先頭1Byteのパケット種類
     public enum PacketType : byte
     {
-        Message        = 0,    //  Stringパケット
-        AssignPlayerId = 1,    //  プレイヤーID割り振りパケット
-        Position       = 2,    //  座標パケット
-        GoMatch        = 3,    //  マッチ通知用パケット
-        MatchSuccess   = 4,    //  マッチ成功パケット
-        SpawnPlayer    = 5,    //  プレイヤー生成パケット
+        Message        = 0,  //  Stringパケット
+        AssignPlayerId = 1,  //  プレイヤーID割り振りパケット
+        Login          = 2,  //  ログイン要求パケット
+        GoMatch        = 3,  //  マッチ通知用パケット
+        MatchSuccess   = 4,  //  マッチ成功パケット
+        SpawnPlayer    = 5,  //  プレイヤー生成パケット
+        Position       = 6,  //  座標パケット
     }
 }
